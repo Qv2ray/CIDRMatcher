a74caea0963c2e6d68d166641e857a0b6b941069
DuckVador <Lx3JQkmzRS@protonmail.com>
2020-11-01 13:15:58 +0800

    add benchmark

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 mod bit_vec;
 mod geoip;
 mod lpc_trie;
+mod cidr_bs;
+
 use crate::lpc_trie::LPCTrie;
 use std::convert::TryInto;
 use std::fs::File;
 use std::net::IpAddr;
+use deepsize::DeepSizeOf;
+use crate::cidr_bs::GeoIPMatcher;
+use crate::geoip::GeoIPList;
+use radix_trie::{Trie, TrieCommon};
+use std::time::SystemTime;
 
 fn main() {
     let file = "src/geoip.dat";
@@ -15,7 +22,7 @@ fn main() {
             return;
         }
     };
-    let site_group_list: geoip::GeoIPList =
+    let mut site_group_list: geoip::GeoIPList =
         match protobuf::parse_from_reader::<geoip::GeoIPList>(&mut f) {
             Ok(v) => v,
             Err(e) => {
@@ -24,36 +31,38 @@ fn main() {
             }
         };
 
-    let mut lpc_trie_cn_us_v6 = LPCTrie::<u128>::new();
+    let mut geoip_matcher = GeoIPMatcher::new();
     let mut lpc_trie_cn_v6 = LPCTrie::<u128>::new();
     let mut lpc_trie_cn_v4 = LPCTrie::<u32>::new();
-    for i in site_group_list.entry.iter() {
+    let mut radix_trie_v6 = Trie::<Vec<u8>,String>::new();
+    let mut radix_trie_v4 = Trie::<u32,String>::new();
+    // insert all cn
+    for i in site_group_list.entry.iter_mut() {
         if i.country_code.to_uppercase() == "CN" {
+            geoip_matcher.put(i);
             for pair in i.cidr.iter() {
                 let len = pair.ip.len();
                 match len {
                     16 => {
                         let inner = pair.ip.clone().try_into().unwrap();
+                        let key =u128::from_be_bytes(inner) >> (128 - pair.prefix)
+                            << (128 - pair.prefix);
                         lpc_trie_cn_v6.put(
-                            u128::from_be_bytes(inner) >> (128 - pair.prefix)
-                                << (128 - pair.prefix),
+                            key,
                             pair.prefix as u8,
                             "CN".to_string(),
                         );
-                        lpc_trie_cn_us_v6.put(
-                            u128::from_be_bytes(inner) >> (128 - pair.prefix)
-                                << (128 - pair.prefix),
-                            pair.prefix as u8,
-                            "CN".to_string(),
-                        );
+                        radix_trie_v6.insert(pair.ip.clone(),"CN".to_string());
                     }
                     4 => {
                         let inner = pair.ip.clone().try_into().unwrap();
+                        let key=u32::from_be_bytes(inner) >> (32 - pair.prefix) << (32 - pair.prefix);
                         lpc_trie_cn_v4.put(
-                            u32::from_be_bytes(inner) >> (32 - pair.prefix) << (32 - pair.prefix),
+                            key,
                             pair.prefix as u8,
                             "CN".to_string(),
                         );
+                        radix_trie_v4.insert(key,"CN".to_string());
                     }
                     _ => {
                         eprintln!("invalid ip length detected");
@@ -62,40 +71,38 @@ fn main() {
             }
         }
     }
-    let mut lpc_trie_us_v6 = LPCTrie::<u128>::new();
-    for i in site_group_list.entry.iter() {
+
+    let repeat_times= 5000;
+    let now=SystemTime::now();
+    for _ in 0..repeat_times {
+        benchmark_lpc(&lpc_trie_cn_v6, &lpc_trie_cn_v4, &site_group_list);
+    }
+    println!("repeat times:{}, benchmark_lpc_matcher:{}", repeat_times,now.elapsed().unwrap().as_secs());
+    let now=SystemTime::now();
+    for _ in 0..repeat_times {
+        benchmark_geoip_matcher(&geoip_matcher, &site_group_list);
+    }
+    println!("repeat times:{},benchmark_core_matcher:{}", repeat_times,now.elapsed().unwrap().as_secs());
+
+    let now=SystemTime::now();
+    for _ in 0..repeat_times {
+        benchmark_radix_trie(&radix_trie_v4, &radix_trie_v6, &site_group_list);
+    }
+    println!("repeat times:{},benchmark_radix_trie:{}", repeat_times, now.elapsed().unwrap().as_secs());
+}
+
+fn benchmark_lpc(lpc_trie_v6:&LPCTrie<u128>,lpc_trie_v4:&LPCTrie<u32>,geoip_list:&GeoIPList){
+    for i in geoip_list.entry.iter() {
         for pair in i.cidr.iter() {
             let len = pair.ip.len();
             match len {
-                16 if i.country_code.to_uppercase() != "CN" => {
-                    let inner = pair.ip.clone().try_into().unwrap();
-                    if i.country_code.to_uppercase() == "US" {
-                        lpc_trie_us_v6.put(
-                            u128::from_be_bytes(inner) >> (128 - pair.prefix)
-                                << (128 - pair.prefix),
-                            pair.prefix as u8,
-                            "US".to_string(),
-                        );
-                        lpc_trie_cn_us_v6.put(
-                            u128::from_be_bytes(inner) >> (128 - pair.prefix)
-                                << (128 - pair.prefix),
-                            pair.prefix as u8,
-                            "US".to_string(),
-                        );
-                    }
-                    assert_eq!(lpc_trie_cn_v6.get(u128::from_be_bytes(inner)), false);
-                }
                 16 => {
                     let inner = pair.ip.clone().try_into().unwrap();
-                    assert_eq!(lpc_trie_cn_v6.get(u128::from_be_bytes(inner)), true);
-                }
-                4 if i.country_code.to_uppercase() != "CN" => {
-                    let inner = pair.ip.clone().try_into().unwrap();
-                    assert_eq!(lpc_trie_cn_v4.get(u32::from_be_bytes(inner)), false);
+                    lpc_trie_v6.get(u128::from_be_bytes(inner));
                 }
                 4 => {
                     let inner = pair.ip.clone().try_into().unwrap();
-                    assert_eq!(lpc_trie_cn_v4.get(u32::from_be_bytes(inner)), true);
+                    lpc_trie_v4.get(u32::from_be_bytes(inner));
                 }
                 _ => {
                     eprintln!("invalid ip length detected");
@@ -103,27 +110,33 @@ fn main() {
             }
         }
     }
-    {
-        if let IpAddr::V4(not_cn_ip) = "8.8.8.8".parse().unwrap() {
-            assert_eq!(
-                lpc_trie_cn_v4.get(u32::from_be_bytes(not_cn_ip.octets())),
-                false
-            );
+}
+
+fn benchmark_geoip_matcher(matcher:&GeoIPMatcher, geoip_list:&GeoIPList) {
+    for i in geoip_list.entry.iter() {
+        for pair in i.cidr.iter() {
+            matcher.match_ip(&pair.ip);
         }
     }
-    {
-        if let IpAddr::V6(us_ip) = "2001:4860:4860::8888".parse().unwrap() {
-            assert_eq!(
-                lpc_trie_us_v6.get(u128::from_be_bytes(us_ip.octets())),
-                true
-            );
-            assert_eq!(
-                lpc_trie_cn_us_v6.get_with_value(u128::from_be_bytes(us_ip.octets())),
-                "US"
-            );
+}
+fn benchmark_radix_trie(trie_v4:&Trie<u32,String>,trie_v6:&Trie<Vec<u8>,String>, geoip_list:&GeoIPList) {
+    for i in geoip_list.entry.iter() {
+        for pair in i.cidr.iter() {
+            let len = pair.ip.len();
+            match len {
+                16 => {
+                    trie_v6.get(&pair.ip);
+                }
+                4 => {
+                    let inner = pair.ip.clone().try_into().unwrap();
+                    let bin_str=(u32::from_be_bytes(inner) >> (32- pair.prefix)
+                        << (32- pair.prefix));
+                    trie_v4.get(&bin_str);
+                }
+                _ => {
+                    eprintln!("invalid ip length detected");
+                }
+            }
         }
     }
-    lpc_trie_cn_us_v6.clear();
-    assert_eq!(lpc_trie_cn_us_v6.empty(), true);
-    println!("Hello, CIDRMatcher!");
 }
